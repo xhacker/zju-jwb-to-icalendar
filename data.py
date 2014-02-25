2586688564e1bc2a8c2f2489bde5baa42b885c3f
Xhacker Liu <liu.dongyuan@gmail.com>
2014-02-25 01:51:21 -0800

    Update week data.
    Need someone inside ZJU to confirm it.

--- data.py
+++ data.py
@@ -4,27 +4,27 @@ from datetime import date, time
 from pytz import timezone
 
 
-'''week_start and week_data ONLY FOR 2013-2014 first semester, should be updated every semester.
+'''week_start and week_data ONLY FOR 2013-2014 second semester, should be updated every semester.
 '''
 
 # date of the first Monday
-week_start = date(2013, 9, 9)
+week_start = date(2014, 2, 24)
 
 week_data = {
-    u"秋": {
-        "odd":  [1, 3, 6, 8],
-        "even": [2, 5, 7, 9],
-        "all":  [1, 2, 3, 5, 6, 7, 8, 9],
+    u"春": {
+        "odd":  [1, 3, 5, 7],
+        "even": [2, 4, 6, 8],
+        "all":  [1, 2, 3, 4, 5, 6, 7, 8],
     },
-    u"冬": {
-        "odd":  [11, 13, 15, 17],
-        "even": [12, 14, 16, 18],
-        "all":  [11, 12, 13, 14, 15, 16, 17, 18],
+    u"夏": {
+        "odd":  [10, 12, 14, 16],
+        "even": [11, 13, 15, 17],
+        "all":  [10, 11, 12, 13, 14, 15, 16, 17],
     },
-    u"秋冬": {
-        "odd":  [1, 3, 6, 8, 11, 13, 15, 17],
-        "even": [2, 5, 7, 9, 12, 14, 16, 18],
-        "all":  [1, 2, 3, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18],
+    u"春夏": {
+        "odd":  [1, 3, 5, 7, 10, 12, 14, 16],
+        "even": [2, 4, 6, 8, 11, 13, 15, 17],
+        "all":  [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17],
     },
 }
 
